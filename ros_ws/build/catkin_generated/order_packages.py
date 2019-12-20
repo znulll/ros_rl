6b09866536231626d5fd777f193b7831d34b9e84
znull <cs.znull@gmail.com>
2019-12-20 22:13:32 +0900

    ros-ws removed

--- ros_ws/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = "/home/donghak/study/ros_rl/ros_ws/src"
-whitelisted_packages = "".split(';') if "" != "" else []
-blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/opt/ros/melodic".split(';') if "/opt/ros/melodic" != "" else []
